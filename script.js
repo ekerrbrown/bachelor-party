1eb3b24cc2d8f2f9e6a180a2995ec162582318bd
ekerrbrown <65882100+ekerrbrown@users.noreply.github.com>
2025-01-12 21:57:35 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,43 +35,43 @@ activityGif.forEach((card) => {
   let cardNumber = card.getAttribute("data-card");
   card.addEventListener("mouseover", function () {
     if (cardNumber == 1) {
-      card.firstElementChild.src = "/images/activities/i-am.gif";
+      card.firstElementChild.src = "images/activities/i-am.gif";
       console.log(card.firstElementChild);
     } else if (cardNumber == 2) {
-      card.firstElementChild.src = "/images/activities/Jason -Mamoa.gif";
+      card.firstElementChild.src = "images/activities/Jason -Mamoa.gif";
     } else if (cardNumber == 3) {
       card.firstElementChild.src =
-        "/images/activities/victoria-pedretti-love.gif";
+        "images/activities/victoria-pedretti-love.gif";
     } else if (cardNumber == 4) {
-      card.firstElementChild.src = "/images/activities/family-guy.gif";
+      card.firstElementChild.src = "images/activities/family-guy.gif";
     } else if (cardNumber == 5) {
-      card.firstElementChild.src = "/images/rules/threat.gif";
+      card.firstElementChild.src = "images/rules/threat.gif";
     } else if (cardNumber == 6) {
-      card.firstElementChild.src = "/images/rules/box-of-shame.gif";
+      card.firstElementChild.src = "images/rules/box-of-shame.gif";
     } else if (cardNumber == 7) {
-      card.firstElementChild.src = "/images/rules/financial.gif";
+      card.firstElementChild.src = "images/rules/financial.gif";
     }
   });
 
   card.addEventListener("mouseout", function () {
     if (cardNumber == 1) {
-      card.firstElementChild.src = "/images/activities/D&Bs Nashville.jpg";
+      card.firstElementChild.src = "images/activities/D&Bs Nashville.jpg";
       console.log(card.firstElementChild);
     } else if (cardNumber == 2) {
-      card.firstElementChild.src = "/images/activities/Axe Throwing.webp";
+      card.firstElementChild.src = "images/activities/Axe Throwing.webp";
     } else if (cardNumber == 3) {
-      card.firstElementChild.src = "/images/activities/powerpoint.jpg";
+      card.firstElementChild.src = "images/activities/powerpoint.jpg";
     } else if (cardNumber == 4) {
-      card.firstElementChild.src = "/images/activities/sports-betting.jpg";
+      card.firstElementChild.src = "images/activities/sports-betting.jpg";
     } else if (cardNumber == 5) {
-      card.firstElementChild.src = "/images/rules/pc.png";
+      card.firstElementChild.src = "images/rules/pc.png";
     } else if (cardNumber == 6) {
-      card.firstElementChild.src = "/images/rules/control-liqueur.webp";
+      card.firstElementChild.src = "images/rules/control-liqueur.webp";
     } else if (cardNumber == 7) {
-      card.firstElementChild.src = "/images/rules/patrick.jpg";
+      card.firstElementChild.src = "images/rules/patrick.jpg";
     }
   });
 });
 
-// src="/images/rules/threat.gif"
-// src="/images/rules/financial.gif"
+// src="images/rules/threat.gif"
+// src="images/rules/financial.gif"
