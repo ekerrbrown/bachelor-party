6c63e713d69c3dce10d874c102e41543d745fc75
ekerrbrown <65882100+ekerrbrown@users.noreply.github.com>
2025-01-12 21:39:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", function () {
 const description = document.querySelectorAll(".flex-row-people");
 const person = document.querySelectorAll(".btn-groom");
 const guitar = document.querySelectorAll("guitar-person");
+const activityGif = document.querySelectorAll(".card");
 
 person.forEach((groom) => {
   groom.addEventListener("click", function () {
@@ -29,3 +30,48 @@ person.forEach((groom) => {
     groom.lastElementChild.classList.toggle("rotate");
   });
 });
+
+activityGif.forEach((card) => {
+  let cardNumber = card.getAttribute("data-card");
+  card.addEventListener("mouseover", function () {
+    if (cardNumber == 1) {
+      card.firstElementChild.src = "/images/activities/i-am.gif";
+      console.log(card.firstElementChild);
+    } else if (cardNumber == 2) {
+      card.firstElementChild.src = "/images/activities/Jason -Mamoa.gif";
+    } else if (cardNumber == 3) {
+      card.firstElementChild.src =
+        "/images/activities/victoria-pedretti-love.gif";
+    } else if (cardNumber == 4) {
+      card.firstElementChild.src = "/images/activities/family-guy.gif";
+    } else if (cardNumber == 5) {
+      card.firstElementChild.src = "/images/rules/threat.gif";
+    } else if (cardNumber == 6) {
+      card.firstElementChild.src = "/images/rules/box-of-shame.gif";
+    } else if (cardNumber == 7) {
+      card.firstElementChild.src = "/images/rules/financial.gif";
+    }
+  });
+
+  card.addEventListener("mouseout", function () {
+    if (cardNumber == 1) {
+      card.firstElementChild.src = "/images/activities/D&Bs Nashville.jpg";
+      console.log(card.firstElementChild);
+    } else if (cardNumber == 2) {
+      card.firstElementChild.src = "/images/activities/Axe Throwing.webp";
+    } else if (cardNumber == 3) {
+      card.firstElementChild.src = "/images/activities/powerpoint.jpg";
+    } else if (cardNumber == 4) {
+      card.firstElementChild.src = "/images/activities/sports-betting.jpg";
+    } else if (cardNumber == 5) {
+      card.firstElementChild.src = "/images/rules/pc.png";
+    } else if (cardNumber == 6) {
+      card.firstElementChild.src = "/images/rules/control-liqueur.webp";
+    } else if (cardNumber == 7) {
+      card.firstElementChild.src = "/images/rules/patrick.jpg";
+    }
+  });
+});
+
+// src="/images/rules/threat.gif"
+// src="/images/rules/financial.gif"
